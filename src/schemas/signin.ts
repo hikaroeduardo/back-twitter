7dd557a06f0ee46e78b52983825dadc4a747a2e4
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-12-16 18:42:24 -0300

    feat: created signin schema

---
+++ src/schemas/signin.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const signinSchema = z.object({
+    email: z
+        .string({ message: "E-mail é obrigatório." })
+        .email("E-mail inválido."),
+    password: z
+        .string({ message: "Senha é obrigatório." })
+        .min(4, "Precisa ter 4 ou mais caracteres."),
+});
