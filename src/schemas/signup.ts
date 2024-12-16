7e08ae14d1fa4dc76bd175dfaef7be3364f82bdd
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-12-16 11:02:15 -0300

    feat: add schema to sign up

---
+++ src/schemas/signup.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+    name: z
+        .string({ message: "Nome é obrigatório." })
+        .min(2, "Precisa ter 2 ou mais caracteres."),
+    email: z
+        .string({ message: "E-mail é obrigatório." })
+        .email("E-mail inválido."),
+    password: z
+        .string({ message: "Senha é obrigatório." })
+        .min(4, "Precisa ter 4 ou mais caracteres."),
+});
