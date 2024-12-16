5f8b4c0ca3ff0cb60ed9179fd66ed4e2d2f0a329
Hikaro Eduardo <hikaroeduardobandeira@gmail.com>
2024-12-16 19:29:17 -0300

    feat: created middleware to requests

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,5 +1,45 @@
+import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+
+import { findUserBySlug } from "../services/user";
+import { ExtendedRequest } from "../types/extend-request";
 
 export const createJWT = (slug: string) => {
     return jwt.sign({ slug }, process.env.JWT_SECRET as string);
+};
+
+export const verifyJWT = (
+    req: ExtendedRequest,
+    res: Response,
+    next: NextFunction
+) => {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+        res.status(401).json({ error: "Acesso negado." });
+        return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(
+        token,
+        process.env.JWT_SECRET as string,
+        async (error, decoded: any) => {
+            if (error) {
+                res.status(401).json({ error: "Acesso negado." });
+                return;
+            }
+
+            const user = await findUserBySlug(decoded.slug);
+
+            if (!user) {
+                res.status(401).json({ error: "Acesso negado." });
+                return;
+            }
+
+            req.userSlug = user.slug;
+
+            next();
+        }
+    );
 };
